f5a37f544d83b7fc41d8dbb37e0eb6edf761d5bc
jeremiahwitt <jeremiah.witt@mail.mcgill.ca>
2017-10-14 17:51:56 -0400

    Added some more to plan algorithm
    fleshed out the algorithm to generate the plan, ever so slightly

--- src/main/java/plan_builder/PlanBuilder.java
+++ src/main/java/plan_builder/PlanBuilder.java
@@ -7,11 +7,17 @@ import race.Race;
  */
 public class PlanBuilder {
 
+    public static final int WEEKLY_INCREASE_MODIFIER = 1;
+    private static final int TARGET_MILEAGE_MODIFIER = 20;
     private Race raceForPlan;
     private TrainingPlanBase planBase;
 
     private double targetLongRun;
     private double baseLongRun;
+    private double longRunIncrease;
+    private double weeklyIncrease;
+    private double targetMileage;
+    private double baseWeeklyDistance;
 
     public PlanBuilder(Race race, TrainingPlanBase basePlan) {
         this.raceForPlan = race;
@@ -19,15 +25,37 @@ public class PlanBuilder {
     }
 
     public void createPlan() {
-        targetLongRun = PlanBuilderHelper.getTargetLongRun(raceForPlan);
-        baseLongRun = PlanBuilderHelper.getBaseLongRun(planBase);
+        this.targetLongRun = PlanBuilderHelper.getTargetLongRun(raceForPlan);
+        this.baseLongRun = PlanBuilderHelper.getBaseLongRun(planBase);
 
         // Then calc base long run, then calc the weekly increase
-        double longRunIncrease = getLongRunIncrease();
+        this.longRunIncrease = getLongRunIncrease();
+        this.weeklyIncrease = getWeeklyIncrease(longRunIncrease);
+        this.targetMileage = getTargetMileage();
+        this.baseWeeklyDistance = getBaseWeeklyDistance();
 
     }
 
+    // TODO comment
+    private double getBaseWeeklyDistance() {
+        return targetMileage - (weeklyIncrease * planBase.getNumWeeks());
+    }
+
+    // TODO comment
+    private double getWeeklyIncrease(double longRunIncrease) {
+        double weeklyIncrease = longRunIncrease + WEEKLY_INCREASE_MODIFIER;
+        // TODO Generate some sort of warning that will get returned if
+        // weeklyIncrease > 3
+        return weeklyIncrease;
+    }
+
+    // TODO comment
     private double getLongRunIncrease() {
         return (targetLongRun - baseLongRun) / planBase.getNumWeeks();
+    }
+
+    // TODO comment
+    private double getTargetMileage() {
+        return raceForPlan.getRaceDistance() + TARGET_MILEAGE_MODIFIER;
     }
 }
